583025c8b1fae2feac871e3ff0a5d01e28307f2c
Gabriel Suarez <budhisadva@gmail.com>
2023-03-18 20:49:44 -0600

    Enviar-Recibir mensajes

--- 01/prac01.c
+++ 01/prac01.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <mpi.h>
+#include <stdlib.h> //contiene rand() y srand()
+#include <time.h> //contiene time() y time_t
 
 #define TAG_MESSAGE 1
 #define TAG_DATA 2
+
+void inicializa(){
+  time_t t;
+  srand(time(&t));
+}
 
 int main(int argc, char** argv) {
   int size;
@@ -13,24 +20,23 @@ int main(int argc, char** argv) {
   //MPI_Isend(cosaAEnviar, tamaño, tipo, recibidor, mensaje, red, almacenSolicitud)
   //MPI_Irecv(almacenDatos, tamaño, tipo, fuenteMen, mensaje, red, almacenSolicitud)
   /*Codigo que ejecuta cada nodo*/
-  int send_data;
+  char send_data;
   int recv_data;
   MPI_Request recv_request;
   MPI_Request send_request;
-  for (int k = 0; k < size; k++) {
-    if (k != rank) {
-      send_data = rank;
-      MPI_Isend(&send_data, 1, MPI_INT, k, TAG_MESSAGE, MPI_COMM_WORLD, &send_request);
-    } else {
-      for (int j = 0; j < size; j++) {
-        if (j != rank) {
-          MPI_Irecv(&recv_data, 1, MPI_INT, j, TAG_MESSAGE, MPI_COMM_WORLD, &recv_request);
-        }
-      }
-    }
+  inicializa();
+  int n = 1 + (rand()%2);
+	if (rank == 0) {
+    send_data = 'A';
+    MPI_Isend(&send_data, 1, MPI_CHAR, n, TAG_MESSAGE, MPI_COMM_WORLD, &send_request);
+    printf("Nodo 0 envio A a nodo %i\n", n);
   }
-  MPI_Waitall(1, &recv_request, MPI_STATUSES_IGNORE);
-	printf("%5i recibio mensaje %i\n", rank, recv_data);
+  if (rank == n) {
+    MPI_Irecv(&recv_data, 1, MPI_CHAR, 0, TAG_MESSAGE, MPI_COMM_WORLD, &recv_request);
+    int terminado;
+    MPI_Waitany(1, &recv_request, &terminado, MPI_STATUSES_IGNORE);
+    printf("\tNodo %i recibio mensaje %c, terminado %i\n", rank, recv_data, terminado);
+  }
   /**/
   MPI_Finalize();
   return 0;
--- 01/run.sh
+++ 01/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # --oversubscribe -np 4
 mpicc prac01.c
-# mpirun a.out
-mpirun --oversubscribe -np 3 a.out
+# mpirun --oversubscribe -np 3 a.out
+for (( i = 0; i < 10; i++ )); do
+  echo "${i}"
+  mpirun --oversubscribe -np 3 a.out
+done
